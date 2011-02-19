c931eb3ded6a9064a97d9cbf47415cf3072e537a
mapseven <cykerstin@googlemail.com>
2011-02-19 21:52:36 +0100

    Added eID

--- ext_localconf.php
+++ ext_localconf.php
@@ -9,6 +9,6 @@ t3lib_extMgm::addTypoScript($_EXTKEY,'editorcfg','
 	tt_content.CSS_editor.ch.tx_geosearch_pi1 = < plugin.tx_geosearch_pi1.CSS_editor
 ',43);
 
-
+$TYPO3_CONF_VARS['FE']['eID_include']['geosearch_srv'] = 'EXT:geosearch/class.tx_geosearch_srv.php';
 t3lib_extMgm::addPItoST43($_EXTKEY,'pi1/class.tx_geosearch_pi1.php','_pi1','list_type',0);
 ?>
